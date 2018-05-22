dbb13e99c070a8c9740ae924ceaa568bd0281fec
josh <josh.fierrosga@gmail.com>
2018-05-22 15:14:32 -0500

    Adding test for EmployeeRows

--- lab-03-routing/src/components/employees/EmployeeRow.test.js
+++ lab-03-routing/src/components/employees/EmployeeRow.test.js
@@ -7,3 +7,19 @@ describe('Employee Row Component: ', () => {
     expect(true).toBe(true);
   });
 });
+
+it('should instantiate the employee table', (){
+  const employee = {
+    username:'fflinstone',
+    email: 'fred.flinstone@slateqarry.com',
+    firstName: 'Fred',
+    lastName: 'Flinstone',
+    admin: true,
+  };
+
+  const component = shallow(<EmployeeRow employee{employee}/>);
+
+  expect(component).toContainReact(<td>Flinstone</td>);
+  expect(component).toContainReact(<td>fflinstone</td>)
+  expect(component).toContainReact(<td>Yes</td>)
+});
